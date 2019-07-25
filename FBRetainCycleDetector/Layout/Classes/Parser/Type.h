797f55b8dbe4f2042d4ed703eae5dcfaf9667693
Andrew Monshizadeh <amonshiz@fb.com>
2019-07-25 16:34:04 -0400

    Build with Xcode 11
    These files were being flagged by the `-Wdefaulted-function-deleted`
    flag.
    
    ```
    FBRetainCycleDetector/Layout/Classes/Parser/Struct.h:31:13: error: explicitly defaulted move assignment operator is implicitly deleted [-Werror,-Wdefaulted-function-deleted]
    FBRetainCycleDetector/Layout/Classes/Parser/Type.h:27:11: error: explicitly defaulted move assignment operator is implicitly deleted [-Werror,-Wdefaulted-function-deleted]
    ```
    
    As these are implicitly deleted, just go ahead and delete them.

--- FBRetainCycleDetector/Layout/Classes/Parser/Type.h
+++ FBRetainCycleDetector/Layout/Classes/Parser/Type.h
@@ -20,19 +20,18 @@ namespace FB { namespace RetainCycleDetector { namespace Parser {
   public:
     const std::string name;
     const std::string typeEncoding;
-    
+
     Type(const std::string &name,
          const std::string &typeEncoding): name(name), typeEncoding(typeEncoding) {}
     Type(Type&&) = default;
-    Type &operator=(Type&&) = default;
-    
+
     Type(const Type&) = delete;
     Type &operator=(const Type&) = delete;
-    
+
     virtual void passTypePath(std::vector<std::string> typePath) {
       this->typePath = typePath;
     }
-    
+
     std::vector<std::string> typePath;
   };
 } } }
