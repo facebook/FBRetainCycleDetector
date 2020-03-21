df50c9eff9e940265298bb57385939434ee9e4df
Max Ovtsin <maxovtsin@fb.com>
2020-03-21 13:09:26 -0700

    Get FBRetainCycleDetector compiled with Clang 10 (#95)
    Summary:
    Pull Request resolved: https://github.com/facebook/FBRetainCycleDetector/pull/95
    
    Fixed compilation errors
    
    Reviewed By: Megra
    
    Differential Revision: D20549976
    
    fbshipit-source-id: 9afed6ab1306c970d729fe5d6357c48bb5994237

--- FBRetainCycleDetector/Layout/Classes/FBClassStrongLayout.mm
+++ FBRetainCycleDetector/Layout/Classes/FBClassStrongLayout.mm
@@ -198,7 +198,7 @@ NSArray<id<FBObjectReference>> *FBGetObjectStrongReferences(id obj,
     if (!ivars) {
       ivars = FBGetStrongReferencesForClass(currentClass);
       if (layoutCache && currentClass) {
-        layoutCache[currentClass] = ivars;
+        layoutCache[(id<NSCopying>)currentClass] = ivars;
       }
     }
     [array addObjectsFromArray:ivars];
