b2d0fe16fdedde3deec746b47559db471d100e6c
Arjun Moudgil <arjunmoudgil@fb.com>
2020-09-24 08:35:44 -0700

    Added check for non-malloced objects in RCD
    Summary:
    For an object that is not created using malloced/realloced, running RCD on it is pointless. Hence adding a condition to check if object we are considering for RCD is malloced or not. Apparently for some classes in iOS 14 (libraries) the objects are not malloc based.
    The classes altered in iOS 14 underneath might be putting the nodes in segments other than heap. Unaware of what all these classes are safest choice is to check if the object is malloced at the first placed, if not ignore it.
    
    Examples:
    `__NSGlobalBlock__`
    (integer constants)
    (string constants)
    Empty arrays `__NSArray0`
    
    Fellow developer on RCD open source, already put up a similar PR for this.
    https://fburl.com/a78ug38s
    Probably merge from there (to encourage it more), unsure about the protocol here.
    
    Differential Revision: D23822041
    
    fbshipit-source-id: 1087030d379590774b1a4db9addf89f5d3a269f8

--- FBRetainCycleDetector/Graph/FBObjectiveCGraphElement.mm
+++ FBRetainCycleDetector/Graph/FBObjectiveCGraphElement.mm
@@ -10,6 +10,7 @@
 
 #import <objc/message.h>
 #import <objc/runtime.h>
+#import <malloc/malloc.h>
 
 #import "FBAssociationManager.h"
 #import "FBClassStrongLayout.h"
@@ -39,22 +40,27 @@
 {
   if (self = [super init]) {
 #if _INTERNAL_RCD_ENABLED
-    // We are trying to mimic how ObjectiveC does storeWeak to not fall into
-    // _objc_fatal path
-    // https://github.com/bavarious/objc4/blob/3f282b8dbc0d1e501f97e4ed547a4a99cb3ac10b/runtime/objc-weak.mm#L369
+    // For an object that is not created using malloc/realloc, running RCD on it is pointless.
+    // Hence adding a condition to check if object we are considering for RCD is malloced or not.
+    malloc_zone_t *zone = malloc_zone_from_ptr((__bridge void *)object);
+    if (zone) {
+      // We are trying to mimic how ObjectiveC does storeWeak to not fall into
+      // _objc_fatal path
+      // https://github.com/bavarious/objc4/blob/3f282b8dbc0d1e501f97e4ed547a4a99cb3ac10b/runtime/objc-weak.mm#L369
 
-    Class aCls = object_getClass(object);
+      Class aCls = object_getClass(object);
 
-    BOOL (*allowsWeakReference)(id, SEL) =
-    (__typeof__(allowsWeakReference))class_getMethodImplementation(aCls, @selector(allowsWeakReference));
+      BOOL (*allowsWeakReference)(id, SEL) =
+      (__typeof__(allowsWeakReference))class_getMethodImplementation(aCls, @selector(allowsWeakReference));
 
-    if (allowsWeakReference && (IMP)allowsWeakReference != _objc_msgForward) {
-      if (allowsWeakReference(object, @selector(allowsWeakReference))) {
-        // This is still racey since allowsWeakReference could change it value by now.
+      if (allowsWeakReference && (IMP)allowsWeakReference != _objc_msgForward) {
+        if (allowsWeakReference(object, @selector(allowsWeakReference))) {
+          // This is still racey since allowsWeakReference could change it value by now.
+          _object = object;
+        }
+      } else {
         _object = object;
       }
-    } else {
-      _object = object;
     }
 #endif
     _namePath = namePath;
