bde2351d49f256bea31b4f3fa53ab24edc532579
Trevor Whittingham <trevor14@meta.com>
2025-05-13 12:32:02 -0700

    Apply old pull request from RCD public repo to fix iOS 18.4 crash
    Summary:
    RCD has been crashing WA on physical devices running iOS 18.4 and 18.4.1. The exact crash reason is highly hard to debug & appears to be a result of a low level memory handling error.
    
    I found [this old PR](https://github.com/facebook/FBRetainCycleDetector/pull/101/files) on the public RCD repo that uses a different method of counting block strong references, and it replaces the crashing line of code. The PR was approved but never merged due to CI issues (likely from the small build errors which I've solved in this diff). I copied PR logic to my local environment & fixed two minor build errors which prevented the merge years ago (`__typeof` rather than `typeof`, and remove an unused method). Now WA does not crash on my iOS 18.4 physical device and RCD appears to catch just as many retain cycles if not more. Code AI confirmed that this fix should work well.
    
    After this lands I need to run the script that compiles RCD for WA so this diff should not change app behavior yet.
    
    Differential Revision: D74517793
    
    fbshipit-source-id: 4d1985a8d7a2f8ddbd2a2e601b697afa23a0c9a9

--- FBRetainCycleDetector/Layout/Blocks/FBBlockInterface.h
+++ FBRetainCycleDetector/Layout/Blocks/FBBlockInterface.h
@@ -19,6 +19,45 @@ enum { // Flags from BlockLiteral
   BLOCK_IS_GLOBAL =         (1 << 28),
   BLOCK_HAS_STRET =         (1 << 29), // IFF BLOCK_HAS_SIGNATURE
   BLOCK_HAS_SIGNATURE =     (1 << 30),
+  BLOCK_HAS_EXTENDED_LAYOUT=(1 << 31)  // compiler
+};
+
+enum {
+  BLOCK_LAYOUT_ESCAPE = 0, // N=0 halt, rest is non-pointer. N!=0 reserved.
+  BLOCK_LAYOUT_NON_OBJECT_BYTES = 1,    // N bytes non-objects
+  BLOCK_LAYOUT_NON_OBJECT_WORDS = 2,    // N words non-objects
+  BLOCK_LAYOUT_STRONG           = 3,    // N words strong pointers
+  BLOCK_LAYOUT_BYREF            = 4,    // N words byref pointers
+  BLOCK_LAYOUT_WEAK             = 5,    // N words weak pointers
+  BLOCK_LAYOUT_UNRETAINED       = 6,    // N words unretained pointers
+  BLOCK_LAYOUT_UNKNOWN_WORDS_7  = 7,    // N words, reserved
+  BLOCK_LAYOUT_UNKNOWN_WORDS_8  = 8,    // N words, reserved
+  BLOCK_LAYOUT_UNKNOWN_WORDS_9  = 9,    // N words, reserved
+  BLOCK_LAYOUT_UNKNOWN_WORDS_A  = 0xA,  // N words, reserved
+  BLOCK_LAYOUT_UNUSED_B         = 0xB,  // unspecified, reserved
+  BLOCK_LAYOUT_UNUSED_C         = 0xC,  // unspecified, reserved
+  BLOCK_LAYOUT_UNUSED_D         = 0xD,  // unspecified, reserved
+  BLOCK_LAYOUT_UNUSED_E         = 0xE,  // unspecified, reserved
+  BLOCK_LAYOUT_UNUSED_F         = 0xF,  // unspecified, reserved
+};
+
+// Values for Block_byref->flags to describe __block variables
+enum {
+  // Byref refcount must use the same bits as Block_layout's refcount.
+  // BLOCK_DEALLOCATING =      (0x0001),  // runtime
+  // BLOCK_REFCOUNT_MASK =     (0xfffe),  // runtime
+
+  BLOCK_BYREF_LAYOUT_MASK =       (0xf << 28), // compiler
+  BLOCK_BYREF_LAYOUT_EXTENDED =   (  1 << 28), // compiler
+  BLOCK_BYREF_LAYOUT_NON_OBJECT = (  2 << 28), // compiler
+  BLOCK_BYREF_LAYOUT_STRONG =     (  3 << 28), // compiler
+  BLOCK_BYREF_LAYOUT_WEAK =       (  4 << 28), // compiler
+  BLOCK_BYREF_LAYOUT_UNRETAINED = (  5 << 28), // compiler
+
+  BLOCK_BYREF_IS_GC =             (  1 << 27), // runtime
+
+  BLOCK_BYREF_HAS_COPY_DISPOSE =  (  1 << 25), // compiler
+  BLOCK_BYREF_NEEDS_FREE =        (  1 << 24), // runtime
 };
 
 struct BlockDescriptor {
@@ -28,6 +67,7 @@ struct BlockDescriptor {
   void (*copy_helper)(void *dst, void *src); // IFF (1<<25)
   void (*dispose_helper)(void *src);         // IFF (1<<25)
   const char *signature;                     // IFF (1<<30)
+  const char *layout;                        // IFF (1<<31)
 };
 
 struct BlockLiteral {
@@ -37,4 +77,13 @@ struct BlockLiteral {
   void (*invoke)(void *, ...);
   struct BlockDescriptor *descriptor;
   // imported variables
+};
+
+struct Block_byref {
+  void *isa;
+  struct Block_byref *forwarding;
+  volatile int32_t flags; // contains ref count
+  uint32_t size;
+  void (*keep_helper)(void *dst, void *src);
+  void (*destory_helper)(void *src);
 };
--- FBRetainCycleDetector/Layout/Blocks/FBBlockStrongLayout.m
+++ FBRetainCycleDetector/Layout/Blocks/FBBlockStrongLayout.m
@@ -25,56 +25,6 @@
  https://github.com/mikeash/Circle
  https://github.com/mikeash/Circle/blob/master/Circle/CircleIVarLayout.m
  */
-static NSIndexSet *_GetBlockStrongLayout(void *block) {
-  struct BlockLiteral *blockLiteral = block;
-
-  /**
-   BLOCK_HAS_CTOR - Block has a C++ constructor/destructor, which gives us a good chance it retains
-   objects that are not pointer aligned, so omit them.
-
-   !BLOCK_HAS_COPY_DISPOSE - Block doesn't have a dispose function, so it does not retain objects and
-   we are not able to blackbox it.
-   */
-  if ((blockLiteral->flags & BLOCK_HAS_CTOR)
-      || !(blockLiteral->flags & BLOCK_HAS_COPY_DISPOSE)) {
-    return nil;
-  }
-
-  void (*dispose_helper)(void *src) = blockLiteral->descriptor->dispose_helper;
-  const size_t ptrSize = sizeof(void *);
-
-  // Figure out the number of pointers it takes to fill out the object, rounding up.
-  const size_t elements = (blockLiteral->descriptor->size + ptrSize - 1) / ptrSize;
-
-  // Create a fake object of the appropriate length.
-  void *obj[elements];
-  void *detectors[elements];
-
-  for (size_t i = 0; i < elements; ++i) {
-    FBBlockStrongRelationDetector *detector = [FBBlockStrongRelationDetector new];
-    obj[i] = detectors[i] = detector;
-  }
-
-  @autoreleasepool {
-    dispose_helper(obj);
-  }
-
-  // Run through the release detectors and add each one that got released to the object's
-  // strong ivar layout.
-  NSMutableIndexSet *layout = [NSMutableIndexSet indexSet];
-
-  for (size_t i = 0; i < elements; ++i) {
-    FBBlockStrongRelationDetector *detector = (FBBlockStrongRelationDetector *)(detectors[i]);
-    if (detector.isStrong) {
-      [layout addIndex:i];
-    }
-
-    // Destroy detectors
-    [detector trueRelease];
-  }
-
-  return layout;
-}
 
 NSArray *FBGetBlockStrongReferences(void *block) {
   if (!FBObjectIsBlock(block)) {
@@ -82,22 +32,60 @@ NSArray *FBGetBlockStrongReferences(void *block) {
   }
 
   NSMutableArray *results = [NSMutableArray new];
-
-  void **blockReference = block;
-  NSIndexSet *strongLayout = _GetBlockStrongLayout(block);
-  [strongLayout enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
-    void **reference = &blockReference[idx];
-
-    if (reference && (*reference)) {
-      id object = (id)(*reference);
-
-      if (object) {
-        [results addObject:object];
+  
+  struct BlockLiteral *blockLiteral = block;
+  
+  if (!(blockLiteral->flags & BLOCK_HAS_EXTENDED_LAYOUT) ||
+      !(blockLiteral->flags & BLOCK_HAS_COPY_DISPOSE)) return results;
+  
+  // If the layout field is less than 0x1000, then it is a compact encoding
+  // of the form 0xXYZ: X strong pointers, then Y byref pointers,
+  // then Z weak pointers.
+  
+  // If the layout field is 0x1000 or greater, it points to a
+  // string of layout bytes. Each byte is of the form 0xPN.
+  // Operator P is from the list below. Value N is a parameter for the operator.
+  // Byte 0x00 terminates the layout; remaining block data is non-pointer bytes.
+  
+  const char *layout = blockLiteral->descriptor->layout;
+  
+  int strongReferenceCount = 0;
+  int byrefReferenceCount = 0;
+  if ((int)layout < 0x1000) {
+    strongReferenceCount = ((int)layout & 0xF00) >> 8;
+    byrefReferenceCount = ((int)layout & 0x0F0) >> 4;
+  } else {
+    for (int i = 0; layout[i] != 0x00; i++) {
+      int p = (layout[i] & 0xF0) >> 4;
+      if (p == BLOCK_LAYOUT_STRONG) {
+        strongReferenceCount += (layout[i] & 0x0F) + 1;
+      } else if (p == BLOCK_LAYOUT_BYREF) {
+        byrefReferenceCount += (layout[i] & 0x0F) + 1;
       }
     }
-  }];
-
-  return [results autorelease];
+  }
+  
+  void *desc = (uint8_t *)block + sizeof(*blockLiteral);
+  
+  if (strongReferenceCount) {
+    for (int i = 0; i < strongReferenceCount; i++, desc += sizeof(void *)) {
+      id strongRef = (__bridge id)(*((void **)desc));
+      if (strongRef) [results addObject:strongRef];
+    }
+  }
+  
+  if (byrefReferenceCount) {
+    for (int i = 0; i < byrefReferenceCount; i++, desc += sizeof(void *)) {
+      struct Block_byref *blockByref = (struct Block_byref *)(*((void **)desc));
+      if ((blockByref->flags & BLOCK_BYREF_HAS_COPY_DISPOSE) && (blockByref->flags & BLOCK_BYREF_LAYOUT_STRONG)) {
+        void *byrefDesc = (uint8_t *)blockByref + sizeof(*blockByref);
+        id strongRef = (__bridge id)(*((void **)byrefDesc));
+        if (strongRef) [results addObject:strongRef];
+      }
+    }
+  }
+  
+  return results;
 }
 
 static Class _BlockClass(void) {
