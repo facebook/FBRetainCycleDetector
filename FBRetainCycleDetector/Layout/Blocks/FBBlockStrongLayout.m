f1a78abc75be78f00a09a3857951905611563b26
Robin Goos <goos@meta.com>
2025-06-27 15:35:03 -0700

    Fix block introspection for blocks with extended layouts
    Summary:
    The block introspection was still breaking for a somewhat rare case where a block was represented with the extended layout. This primarily happens either when the reference count of a specific kind (i.e. strong, weak, __block) can't fit in 4 bits, or if it captures anything other than those three (e.g. non-objects).
    
    When this happens, the block storage (i.e. the chunk of memory after the block literal struct) looks different than it does for the compact layouts in the sense that strong objects can be interleaved with __block objects, rather than being listed in the oder of strong, __block, weak. It can also interleave non-objects in there, which is fun.
    
    This means that the logic we used before—which was the same for compact and extended layouts—would fail whenever it found these interleaved types.
    
    To get around that this diff separates the logic for each block layout, and updates the extended block layout's logic to collect the objects as it's iterating through the layout, so that it can increment the word offset even for captured objects and values we don't care about.
    
    Reviewed By: twhitt14
    
    Differential Revision: D77424649
    
    fbshipit-source-id: cbff553b75b47255be1be0d66bad7bad0231b77f

--- FBRetainCycleDetector/Layout/Blocks/FBBlockStrongLayout.m
+++ FBRetainCycleDetector/Layout/Blocks/FBBlockStrongLayout.m
@@ -26,6 +26,82 @@
  https://github.com/mikeash/Circle/blob/master/Circle/CircleIVarLayout.m
  */
 
+static NSArray *_GetStrongReferencesCompactLayout(struct BlockLiteral *blockLiteral) {
+  NSMutableArray *strongReferences = [NSMutableArray array];
+
+  const char *blockLayout = blockLiteral->descriptor->layout;
+
+  int strongReferenceCount = ((uintptr_t)blockLayout & 0xF00) >> 8;
+  int byrefReferenceCount = ((uintptr_t)blockLayout & 0x0F0) >> 4;
+
+  uintptr_t *storagePointer = (uintptr_t *)((uintptr_t)blockLiteral + sizeof(*blockLiteral));
+  if (strongReferenceCount > 0) {
+    for (int i = 0; i < strongReferenceCount; i += 1, storagePointer += 1) {
+      id strongRef = (__bridge id)(*((void **)storagePointer));
+      if (strongRef) {
+        [strongReferences addObject:strongRef];
+      }
+    }
+  }
+
+  if (byrefReferenceCount > 0) {
+    for (int i = 0; i < byrefReferenceCount; i += 1, storagePointer += 1) {
+      struct Block_byref *blockByref = (struct Block_byref *)(*((void **)storagePointer));
+      BOOL isStrongLayout = (blockByref->flags & BLOCK_BYREF_LAYOUT_MASK) == BLOCK_BYREF_LAYOUT_STRONG;
+      BOOL hasCopyDispose = blockByref->flags & BLOCK_BYREF_HAS_COPY_DISPOSE;
+      if (hasCopyDispose && isStrongLayout) {
+        void *byrefDesc = (uint8_t *)blockByref + sizeof(*blockByref);
+        id strongRef = (__bridge id)(*((void **)byrefDesc));
+        if (strongRef) {
+          [strongReferences addObject:strongRef];
+        }
+      }
+    }
+  }
+
+  return strongReferences;
+}
+
+static NSArray *_GetStrongReferencesExtendedLayout(struct BlockLiteral *blockLiteral)
+{
+  NSMutableArray *strongReferences = [NSMutableArray array];
+  const char *blockLayout = blockLiteral->descriptor->layout;
+
+  uintptr_t *storagePointer = (uintptr_t *)((uintptr_t)blockLiteral + sizeof(*blockLiteral));
+  uintptr_t wordOffset = 0;
+
+  for (int i = 0; blockLayout[i] != 0x00; i++) {
+    int p = (blockLayout[i] & 0xF0) >> 4;
+    int n = (blockLayout[i] & 0x0F) + 1;
+    if (p == BLOCK_LAYOUT_STRONG) {
+      for (int j = 0; j < n; j++) {
+        void *ptr = ((uintptr_t *)storagePointer + wordOffset + j);
+        id strongRef = (__bridge id)(*((void **)ptr));
+        if (strongRef) {
+          [strongReferences addObject:strongRef];
+        }
+      }
+    } else if (p == BLOCK_LAYOUT_BYREF) {
+      for (int j = 0; j < n; j++) {
+        uintptr_t *ptr = ((uintptr_t *)storagePointer + wordOffset + j);
+        struct Block_byref *blockByref = (struct Block_byref *)(*((void **)ptr));
+        BOOL isStrongLayout = (blockByref->flags & BLOCK_BYREF_LAYOUT_MASK) == BLOCK_BYREF_LAYOUT_STRONG;
+        BOOL hasCopyDispose = blockByref->flags & BLOCK_BYREF_HAS_COPY_DISPOSE;
+        if (hasCopyDispose && isStrongLayout) {
+          void *byrefPtr = (uint8_t *)blockByref + sizeof(*blockByref);
+          id strongRef = (__bridge id)(*((void **)byrefPtr));
+          if (strongRef) {
+            [strongReferences addObject:strongRef];
+          }
+        }
+      }
+    }
+    wordOffset += n;
+  }
+
+  return strongReferences;
+}
+
 NSArray *FBGetBlockStrongReferences(void *block) {
   if (!FBObjectIsBlock(block)) {
     return nil;
@@ -46,48 +122,12 @@ NSArray *FBGetBlockStrongReferences(void *block) {
   // string of layout bytes. Each byte is of the form 0xPN.
   // Operator P is from the list below. Value N is a parameter for the operator.
   // Byte 0x00 terminates the layout; remaining block data is non-pointer bytes.
-  
   const char *layout = blockLiteral->descriptor->layout;
-  
-  int strongReferenceCount = 0;
-  int byrefReferenceCount = 0;
   if ((int)layout < 0x1000) {
-    strongReferenceCount = ((int)layout & 0xF00) >> 8;
-    byrefReferenceCount = ((int)layout & 0x0F0) >> 4;
+    return _GetStrongReferencesCompactLayout(blockLiteral);
   } else {
-    for (int i = 0; layout[i] != 0x00; i++) {
-      int p = (layout[i] & 0xF0) >> 4;
-      if (p == BLOCK_LAYOUT_STRONG) {
-        strongReferenceCount += (layout[i] & 0x0F) + 1;
-      } else if (p == BLOCK_LAYOUT_BYREF) {
-        byrefReferenceCount += (layout[i] & 0x0F) + 1;
-      }
-    }
+    return _GetStrongReferencesExtendedLayout(blockLiteral);
   }
-  
-  void *desc = (uint8_t *)block + sizeof(*blockLiteral);
-  
-  if (strongReferenceCount) {
-    for (int i = 0; i < strongReferenceCount; i++, desc += sizeof(void *)) {
-      id strongRef = (__bridge id)(*((void **)desc));
-      if (strongRef) [results addObject:strongRef];
-    }
-  }
-  
-  if (byrefReferenceCount) {
-    for (int i = 0; i < byrefReferenceCount; i++, desc += sizeof(void *)) {
-      struct Block_byref *blockByref = (struct Block_byref *)(*((void **)desc));
-      BOOL isStrongLayout = (blockByref->flags & BLOCK_BYREF_LAYOUT_MASK) == BLOCK_BYREF_LAYOUT_STRONG;
-      BOOL hasCopyDispose = blockByref->flags & BLOCK_BYREF_HAS_COPY_DISPOSE;
-      if (hasCopyDispose && isStrongLayout) {
-        void *byrefDesc = (uint8_t *)blockByref + sizeof(*blockByref);
-        id strongRef = (__bridge id)(*((void **)byrefDesc));
-        if (strongRef) [results addObject:strongRef];
-      }
-    }
-  }
-  
-  return results;
 }
 
 static Class _BlockClass(void) {
--- FBRetainCycleDetectorTests/FBBlockStrongLayoutTests.mm
+++ FBRetainCycleDetectorTests/FBBlockStrongLayoutTests.mm
@@ -23,26 +23,26 @@
 {
   __attribute__((objc_precise_lifetime)) NSObject *object = [NSObject new];
   __weak NSObject *weakObject = object;
-  
+
   void (^block)() = ^{
     __unused NSObject *someObject = weakObject;
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 0);
 }
 
 - (void)testBlockRetainsStrongReference
 {
   NSObject *object = [NSObject new];
-  
+
   void (^block)() = ^{
     __unused NSObject *someObject = object;
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 1);
   XCTAssertEqualObjects(retainedObjects[0], object);
 }
@@ -51,13 +51,13 @@
 {
   NSObject *object = [NSObject new];
   std::vector<id> vector = {object};
-  
+
   void (^block)() = ^{
     __unused std::vector<id> someVector = vector;
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 0);
 }
 
@@ -65,27 +65,27 @@
 {
   struct HelperStruct {};
   std::vector<HelperStruct> vector = {};
-  
+
   void (^block)() = ^{
     __unused std::vector<HelperStruct> someVector = vector;
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 0);
 }
 
 - (void)testThatBlockUsingCppButRetainingOnlyObjectsWillReturnTheObjectAndNotCrash
 {
   NSObject *object = [NSObject new];
-  
+
   void (^block)() = ^{
     std::vector<id> vector;
     vector.push_back(object);
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 1);
   XCTAssertEqualObjects(retainedObjects[0], object);
 }
@@ -94,41 +94,48 @@
 {
   struct HelperStruct{};
   std::unordered_map<int, HelperStruct> map;
-  
+
   void (^block)() = ^{
     __unused std::unordered_map<int, HelperStruct> someMap = map;
   };
-  
+
   NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
-  
+
   XCTAssertEqual([retainedObjects count], 0);
 }
 
+typedef struct {
+  id object;
+  int identifier;
+} FBRetainCycleDetectorTestStruct;
+
 #define newObject(A) NSObject *object##A = [NSObject new]
 #define __blockNewObject(A) __block NSObject *object##A = [NSObject new]
+#define newStruct(A) FBRetainCycleDetectorTestStruct struct##A { .object = object##A, .identifier = A }
 #define useObject(A) __unused NSObject *someObject##A = object##A
+#define useStruct(A) __unused FBRetainCycleDetectorTestStruct someStruct##A = { .object = struct##A.object, .identifier = struct##A.identifier }
 #define assertObject(A) XCTAssertEqualObjects(retainedObjects[A], object##A);
 
 - (void)testBlockRetains15StrongReferences
 {
-    
+
     newObject(0); newObject(1); newObject(2); newObject(3);
     newObject(4); newObject(5); newObject(6); newObject(7);
-    
+
     __blockNewObject(8); __blockNewObject(9); __blockNewObject(10); __blockNewObject(11);
     __blockNewObject(12); __blockNewObject(13); __blockNewObject(14);
-    
-    
+
+
     void (^block)() = ^{
         useObject(0); useObject(1); useObject(2); useObject(3);
         useObject(4); useObject(5); useObject(6); useObject(7);
         useObject(8); useObject(9); useObject(10); useObject(11);
         useObject(12); useObject(13); useObject(14);
     };
-    
+
     NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
     XCTAssertEqual([retainedObjects count], 15);
-    
+
     assertObject(0); assertObject(1); assertObject(2); assertObject(3);
     assertObject(4); assertObject(5); assertObject(6); assertObject(7);
     assertObject(8); assertObject(9); assertObject(10); assertObject(11);
@@ -139,20 +146,84 @@
 {
     newObject(0); newObject(1); newObject(2); newObject(3);
     newObject(4); newObject(5); newObject(6); newObject(7);
-    
+
     __blockNewObject(8); __blockNewObject(9); __blockNewObject(10); __blockNewObject(11);
     __blockNewObject(12); __blockNewObject(13); __blockNewObject(14); __blockNewObject(15);
-    
+
     void (^block)() = ^{
         useObject(0); useObject(1); useObject(2); useObject(3);
         useObject(4); useObject(5); useObject(6); useObject(7);
         useObject(8); useObject(9); useObject(10); useObject(11);
         useObject(12); useObject(13); useObject(14); useObject(15);
     };
-    
+
     NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
     XCTAssertEqual([retainedObjects count], 16);
-    
+
+    assertObject(0); assertObject(1); assertObject(2); assertObject(3);
+    assertObject(4); assertObject(5); assertObject(6); assertObject(7);
+    assertObject(8); assertObject(9); assertObject(10); assertObject(11);
+    assertObject(12); assertObject(13); assertObject(14); assertObject(15);
+}
+
+- (void)testBlockRetainsMoreThan15StrongReferencesInterspersedWithBlockReferences
+{
+    newObject(0); newObject(1); newObject(2); newObject(3);
+    newObject(4); newObject(5); newObject(6); newObject(7);
+    newObject(8); newObject(9); newObject(10); newObject(11);
+    newObject(12); newObject(13); newObject(14); newObject(15);
+
+    __blockNewObject(16); __blockNewObject(17); __blockNewObject(18); __blockNewObject(19);
+    __blockNewObject(20); __blockNewObject(21); __blockNewObject(22); __blockNewObject(23);
+    __blockNewObject(24); __blockNewObject(25); __blockNewObject(26); __blockNewObject(27);
+
+    void (^block)() = ^{
+        useObject(0); useObject(27); useObject(1); useObject(26);
+        useObject(2); useObject(25); useObject(3); useObject(24);
+        useObject(4); useObject(23); useObject(5); useObject(22);
+        useObject(6); useObject(21); useObject(7); useObject(20);
+        useObject(8); useObject(19); useObject(9); useObject(18);
+        useObject(10); useObject(17); useObject(11); useObject(16);
+        useObject(12); useObject(15); useObject(13); useObject(14);
+    };
+
+    NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
+    XCTAssertEqual([retainedObjects count], 28);
+    NSArray *expected = @[
+        object0, object1, object2, object3,
+        object4, object5, object6, object7,
+        object8, object9, object10, object11,
+        object12, object15, object13, object14,
+        object27, object26, object25, object24,
+        object23, object22, object21, object20,
+        object19, object18, object17, object16
+    ];
+    [expected enumerateObjectsUsingBlock:^(NSObject *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+      XCTAssertEqual(obj, retainedObjects[idx], @"Object %@ at index %lu is not equal to %@", obj, idx, retainedObjects[idx]);
+    }];
+}
+
+- (void)testBlockRetainsObjectsStoredInsideStructs
+{
+    newObject(0); newObject(1); newObject(2); newObject(3);
+    newObject(4); newObject(5); newObject(6); newObject(7);
+    newObject(8); newObject(9); newObject(10); newObject(11);
+    newObject(12); newObject(13); newObject(14); newObject(15);
+    newStruct(0); newStruct(1); newStruct(2); newStruct(3);
+    newStruct(4); newStruct(5); newStruct(6); newStruct(7);
+    newStruct(8); newStruct(9); newStruct(10); newStruct(11);
+    newStruct(12); newStruct(13); newStruct(14); newStruct(15);
+
+    void (^block)() = ^{
+      useStruct(0); useStruct(1); useStruct(2); useStruct(3);
+      useStruct(4); useStruct(5); useStruct(6); useStruct(7);
+      useStruct(8); useStruct(9); useStruct(10); useStruct(11);
+      useStruct(12); useStruct(13); useStruct(14); useStruct(15);
+    };
+
+    NSArray *retainedObjects = FBGetBlockStrongReferences((__bridge void *)(block));
+    XCTAssertEqual([retainedObjects count], 16);
+
     assertObject(0); assertObject(1); assertObject(2); assertObject(3);
     assertObject(4); assertObject(5); assertObject(6); assertObject(7);
     assertObject(8); assertObject(9); assertObject(10); assertObject(11);
